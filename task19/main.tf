62cdb10506d225e03c4ce55a8dc8cc397d6bdcc1
OlgaLeeA <130886455+OlgaLeeA@users.noreply.github.com>
2023-05-29 14:20:39 +0900

    Update main.tf

--- task19/main.tf
+++ task19/main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "web_app_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    
+
   ingress {
     description      = "HTTPS"
     from_port        = 443
@@ -46,8 +46,8 @@ resource "aws_security_group" "web_app_security_group" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  } 
-    
+  }
+
   ingress {
     description      = "HTTP"
     from_port        = 80
@@ -55,8 +55,8 @@ resource "aws_security_group" "web_app_security_group" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
-    
+  } 
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -64,7 +64,7 @@ resource "aws_security_group" "web_app_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-    
+}
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-0c9c942bd7bf113a2"
