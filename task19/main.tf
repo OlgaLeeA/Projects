ce7dbaee3759e246cdeb67fde7e4dc1acfb41986
OlgaLeeA <130886455+OlgaLeeA@users.noreply.github.com>
2023-05-29 14:19:16 +0900

    Update main.tf

--- task19/main.tf
+++ task19/main.tf
@@ -37,7 +37,6 @@ resource "aws_security_group" "web_app_security_group" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
     
   ingress {
