3d480b837799dd4bcb49631f8a55f90fda5cac37
OlgaLeeA <130886455+OlgaLeeA@users.noreply.github.com>
2023-05-29 18:53:14 +0900

    Update main.tf

--- task19/main.tf
+++ task19/main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-    region = "ap-northeast-2"
+  region = "ap-northeast-2"
 }
 
-resource "aws_instance" "my_instance" {
-    ami = "data.aws_ami.ubuntu.id"
-    instance_type = "t2.micro"
-    tags = {
-      "Name" = "myEC2"
-    }
- 
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c574c8"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "myEC2"
+  }
 }
 
 output "instance_public_ip" {
   value = aws_instance.example.public_ip
-}  
-  
+}
+
 
