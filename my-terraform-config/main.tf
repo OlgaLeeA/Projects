83bcef6eed54cbbc2d450d16504225cc713b2ad2
OlgaLeeA <130886455+OlgaLeeA@users.noreply.github.com>
2023-05-31 12:56:59 +0900

    Create main.tf

---
+++ my-terraform-config/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-northeast-3"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0997b4797ae01c920"
+  key_name      = "my-key"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "myEC2"
+  }
+}
+
+
+
